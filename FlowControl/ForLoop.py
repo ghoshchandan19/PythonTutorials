b004152c32485d9ce587acf45054682a19eb2580
Chandan Ghosh <ghoshchandan19@gmail.com>
2019-06-13 14:14:30 +0530

    Added for loop

--- FlowControl/ForLoop.py
+++ FlowControl/ForLoop.py
@@ -1,7 +1,19 @@
 #Program to print the sum
 
 numbers=[2,8,9,7,18]
+print(len(numbers))
 sum=0
 for val in numbers:
     sum=sum+val
 print("Total sum is ",sum)
+
+#Iterate through loops
+songs=["Rock","Pop","jazz"]
+for i in range(len(songs)):
+    print("I like",songs[i])
+
+digit=[0,1,3,5,7]
+for i in  digit:
+    print(i)
+else:
+    print("No items left")
